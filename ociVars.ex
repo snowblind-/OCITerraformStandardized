470b9e94e4ba52e99aa2523065963c8e579463f4
snowblind- <snowblind-@users.noreply.github.com>
2020-02-12 16:00:25 -0800

    Create ociVars.ex

---
+++ ociVars.ex
@@ -0,0 +1,18 @@
+# Configure the Oracle Cloud Infrastructure provider with an API Key
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region = "${var.region}"
+}
+
+# Get a list of Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "${var.tenancy_ocid}"
+}
+
+# Output the result
+output "show-ads" {
+  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
+}
